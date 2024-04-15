9bff969d222048fff6efd10c96b7c605222427fa
Nidhiben Patel <pate1293@algonquinlive.com>
2024-04-15 18:46:15 -0400

    committing the code of terraform

---
+++ terraform.tf
@@ -0,0 +1,31 @@
+provider "azurerm" {
+  features {}
+  use_oidc = true
+}
+
+terraform {
+  required_version = "~> 1.5"
+
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "3.96.0"
+    }
+  }
+}
+
+module "backend" {
+  source = "./infrastructure/backend"
+}
+
+module "network" {
+  source = "./infrastructure/network"
+}
+
+module "aks" {
+  source = "./infrastructure/aks"
+}
+
+module "reddis" {
+  source = "./infrastructure/reddis"
+}
